d252bdb2e976946b7e0a1c9c29e0cdec017673d9
Carlos <carlosbolorino@hotmail.com>
2021-03-22 19:11:31 +0100

    Update Formulario.js

--- components/Formulario.js
+++ components/Formulario.js
@@ -2,10 +2,21 @@ import React from 'react'
 import { View, Text, StyleSheet} from 'react-native'
 import { Picker } from '@react-native-picker/picker'
 const Formulario = () => {
+    const [moneda, setMoneda] = useState('');
+    const [cryptoMoneda, setcryptoMoneda] = useState('');
+
+    const obtenerMoneada = moneda => {
+        setMoneda(moneda);
+    }
+
     return (
         <View>
             <Text style={styles.label}>Moneda</Text>
-            <Picker>
+            <Picker
+                selectedValue={moneda}
+                onValueChange={moneda => obtenerMoneda(moneda)}
+            >
+
                 <Picker.Item label="- Seleccione -" value=""/>
                 <Picker.Item label="Dolar EEUU" value="USD"/>
                 <Picker.Item label="Peso Mexicano" value="MXN"/>
